2902c785eaa3a1c27dc1a91feefb84f90a8ddecb
g33kyshivam <shivam.goyal397@gmail.com>
2018-02-03 23:09:55 +0530

    article model+migration+routes

---
+++ db/migrate/20180203172836_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :author
+      t.string :content
+
+      t.timestamps null: false
+    end
+  end
+end
