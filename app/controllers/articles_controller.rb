acad61e0bb39169d6584f6ad116ac32f349adc8a
g33kyshivam <shivam.goyal397@gmail.com>
2018-02-04 00:00:54 +0530

    create action + form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
     def index
-        
+        @article = Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.create(article_params)
+        redirect_to action: 'index'
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:author, :content)
+    end
+
 end
+
